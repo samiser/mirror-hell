0da1fe792523f985686e6b120052c3ddc953373b
wookbee <159189510+wookbee@users.noreply.github.com>
2026-04-20 00:07:38 +0100

    player clamped to the screen

--- player/player.gd
+++ player/player.gd
@@ -2,3 +2,5 @@ extends Node2D
 
 func _process(delta: float) -> void:
 	position = get_global_mouse_position()
+	position.x = clampf(position.x, 20, 628)
+	position.y = clampf(position.y, 30, 1122)
--- player/reflection.gd
+++ player/reflection.gd
@@ -2,3 +2,5 @@ extends Node2D
 
 func _process(delta: float) -> void:
 	position = get_global_mouse_position() * Vector2(-1, 1) + Vector2(get_viewport_rect().size.x, 0)
+	position.x = clampf(position.x, 20, 628)
+	position.y = clampf(position.y, 30, 1122)
