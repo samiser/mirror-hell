557d3bc0ed057e85144059d34812d72da280d972
Samiser <github@me.samiser.xyz>
2026-04-19 20:09:55 +0000

    better wave logic

--- wave_spawner.gd
+++ wave_spawner.gd
@@ -6,17 +6,20 @@ const ENEMY_BASIC = preload("res://enemy/enemy_basic.tres")
 const ENEMY_FAST = preload("res://enemy/enemy_fast.tres")
 const ENEMY_TANK = preload("res://enemy/enemy_tank.tres")
 
+enum State { SPAWNING, WAITING, PAUSED }
+
 var spawn_interval: float = 2.0
 var spawn_margin: float = 100.0
 var spawn_y: float = -100.0
 
 var spawns_per_wave: int = 5
-var wave_pause: float = 10.0
+var wave_pause: float = 3.0
 
 var _spawn_timer: float = 0.0
 var _spawns_this_wave: int = 0
 var _current_wave: int = 0
-var _paused: bool = false
+var _state: State = State.SPAWNING
+var _alive_enemies: int = 0
 
 var _wave_configs: Array[Array] = [
 	[ENEMY_BASIC],
@@ -28,22 +31,30 @@ var _wave_configs: Array[Array] = [
 func _process(delta: float) -> void:
 	_spawn_timer += delta
 
-	if _paused:
-		if _spawn_timer >= wave_pause:
-			_spawn_timer = 0.0
-			_paused = false
-			_current_wave += 1
-		return
+	match _state:
+		State.SPAWNING:
+			if _spawn_timer >= spawn_interval:
+				_spawn_timer -= spawn_interval
+				_spawn_enemy()
+				_spawns_this_wave += 1
 
-	if _spawn_timer >= spawn_interval:
-		_spawn_timer -= spawn_interval
-		_spawn_enemy()
-		_spawns_this_wave += 1
+				if _spawns_this_wave >= spawns_per_wave:
+					_spawns_this_wave = 0
+					_state = State.WAITING
 
-		if _spawns_this_wave >= spawns_per_wave:
-			_spawns_this_wave = 0
-			_spawn_timer = 0.0
-			_paused = true
+		State.WAITING:
+			if _alive_enemies <= 0:
+				_spawn_timer = 0.0
+				_state = State.PAUSED
+
+		State.PAUSED:
+			if _spawn_timer >= wave_pause:
+				_spawn_timer = 0.0
+				_current_wave += 1
+				_state = State.SPAWNING
+
+func _on_enemy_died() -> void:
+	_alive_enemies -= 1
 
 func _get_enemy_data() -> EnemyData:
 	var wave_index := mini(_current_wave, _wave_configs.size() - 1)
@@ -59,10 +70,14 @@ func _spawn_enemy() -> void:
 	var enemy1: Enemy = ENEMY.instantiate()
 	enemy1.apply_data(data)
 	enemy1.position = Vector2(x, spawn_y)
+	enemy1.tree_exited.connect(_on_enemy_died)
 	add_child(enemy1)
+	_alive_enemies += 1
 
 	var enemy2: Enemy = ENEMY.instantiate()
 	enemy2.apply_data(data)
 	enemy2.type = Enemy.Type.RED
 	enemy2.position = Vector2(reflected_x, spawn_y)
+	enemy2.tree_exited.connect(_on_enemy_died)
 	add_child(enemy2)
+	_alive_enemies += 1
